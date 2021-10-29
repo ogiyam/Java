2adba32b6c0c3805d5213007743ea803127a4c01
ogiyam <miyuogym@gmail.com>
2021-10-29 23:50:06 +0900

    [Fix]基礎

---
+++ basic/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 1000L;
+		System.out.println(num1);
+		double num2 = 1.11d;
+		System.out.println(num2);
+		boolean b = true;
+		System.out.println(b);
+		
+		Integer num3 = 1000;
+		System.out.println(num3);
+		Float num4 = 1.11f;
+		System.out.println(num4);
+		Character c = 'あ';
+		System.out.println(c);
+		
+		int[] array = {0,1,2,3,4};
+		System.out.println(array[3]);
+	}
+}
